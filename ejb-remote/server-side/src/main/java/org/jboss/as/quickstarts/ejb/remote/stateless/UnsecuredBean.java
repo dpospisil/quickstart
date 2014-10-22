88b55f09af437ee74819e212f28bf84fb5c60b83
Dominik Pospisil <dpospisi@redhat.com>
2014-10-22 09:01:30 +0200

    update.

--- ejb-remote/server-side/src/main/java/org/jboss/as/quickstarts/ejb/remote/stateless/UnsecuredBean.java
+++ ejb-remote/server-side/src/main/java/org/jboss/as/quickstarts/ejb/remote/stateless/UnsecuredBean.java
@@ -8,7 +8,9 @@ package org.jboss.as.quickstarts.ejb.remote.stateless;
 import javax.ejb.EJB;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.security.auth.login.LoginContext;
 import org.jboss.as.quickstarts.ejb.remote.secured.RemoteEcho;
+import org.jboss.security.auth.callback.UsernamePasswordHandler;
 import org.jboss.security.client.SecurityClient;
 import org.jboss.security.client.SecurityClientFactory;
 
@@ -23,15 +25,40 @@ public class UnsecuredBean implements RemoteEcho {
     @EJB(beanName = "SecuredBean")
     RemoteEcho secured;
 
+    private static final String USER = "guest";
+    private static final String PASSWD = "kilo_maso1";
+
     @Override
     public String secureEcho(String word) {
+        return type2(word);
+    }
 
+    private String type1(String word) {
         try {
+
             SecurityClient client = SecurityClientFactory.getSecurityClient();
-            client.setSimple("user", "mypass");
+            client.setSimple(USER, PASSWD);
             client.login();
 
-            return secured.secureEcho(word);
+            String response = secured.secureEcho(word);
+
+            client.logout();
+
+            return response;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String type2(String word) {
+        try {
+        UsernamePasswordHandler handler = new UsernamePasswordHandler(USER, PASSWD);
+        LoginContext lc = new LoginContext("client-login", handler);
+        lc.login();
+            String response = secured.secureEcho(word);
+        lc.logout();
+        return response;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
